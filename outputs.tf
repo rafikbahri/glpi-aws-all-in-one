d3e987943e9d37009e5593d5b4034f9c2344a5f9
Rafik Bahri <rbahri@feedgy.solar>
2025-07-14 21:48:39 +0200

    feat: Add basic bastion setup

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,53 @@
-output "public_ip" {
-  description = "Public IP of the created EC2 instance"
-  value       = module.bastions.public_ip
+output "bastion_public_ip" {
+  description = "Public IP of bastion host"
+  value       = aws_instance.bastion.public_ip
 }
 
-output "bastions_private_ips" {
-  description = "Private IPs of the bastion servers"
-  value       = module.bastions.private_ips
+output "bastion_public_dns" {
+  description = "Public DNS of bastion host"
+  value       = aws_instance.bastion.public_dns
 }
 
-output "glpi_private_ips" {
-  value = module.glpi-cluster.private_ips
+output "glpi_private_ip" {
+  description = "Private IP of GLPI instance"
+  value       = aws_instance.glpi_instance.private_ip
 }
 
-output "private_key" {
-  description = "Private key for SSH access to the bastion servers"
-  value       = var.create_key ? module.bastions.private_key : null
-  sensitive   = true
+output "ssh_commands" {
+  description = "SSH commands to access instances"
+  value = {
+    bastion   = "ssh -i ~/.ssh/id_ed25519_bastion ec2-user@${aws_instance.bastion.public_ip}"
+    glpi_jump = "ssh -i ~/.ssh/id_ed25519_bastion -J ec2-user@${aws_instance.bastion.public_ip} ec2-user@${aws_instance.glpi_instance.private_ip}"
+  }
 }
 
-output "ssh_command" {
-  description = "SSH command to connect to the bastion servers"
-  value       = "ssh -i ~/.ssh/id_ed25519_bastion -F .ssh/config ec2-user@${module.bastions.hostnames[0]}"
+output "ssh_config" {
+  description = "SSH config file entries"
+  value       = <<-EOF
+Host bastion
+    HostName ${aws_instance.bastion.public_ip}
+    User ec2-user
+    IdentityFile ~/.ssh/id_ed25519_bastion
+
+Host glpi
+    HostName ${aws_instance.glpi_instance.private_ip}
+    User ec2-user
+    IdentityFile ~/.ssh/id_ed25519_bastion
+    ProxyJump bastion
+EOF
+}
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  description = "ID of the private subnet"
+  value       = aws_subnet.private.id
 }
