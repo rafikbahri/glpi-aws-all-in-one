f0cddb3058c8f7ef4473e86dcc68d529120ee152
Rafik Bahri <rbahri@feedgy.solar>
2025-07-14 11:59:02 +0200

    feat: Use rafikbahri tf modules

--- variables.tf
+++ variables.tf
@@ -4,6 +4,12 @@ variable "region" {
   description = "AWS region"
 }
 
+variable "vpc_name" {
+  type        = string
+  default     = "glpi-vpc"
+  description = "Name of the VPC"
+}
+
 variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
